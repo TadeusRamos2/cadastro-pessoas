480fe260a5942ea565b9b84275c544adeeadaac7
Tadeu Ramos <tadeuramos2@gmail.com>
2026-04-15 17:51:50 -0300

    melhorei sistema de cadastro

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let pessoas = [];
 
+let editandoIndex = null;
+
 let dados = localStorage.getItem("pessoas");
 
 if (dados) {
@@ -8,30 +10,41 @@ if (dados) {
 
 atualizarLista();
 
+function editar(index) {
+    let pessoa = pessoas[index];
+
+    document.getElementById("nome").value = pessoa.nome;
+    document.getElementById("idade").value = pessoa.idade;
+
+    editandoIndex = index;
+}
+
 function cadastrar() {
     let nome = document.getElementById("nome").value;
     let idade = document.getElementById("idade").value;
 
     if (!nome || !idade) {
-    alert("Preencha todos os campos!");
-    return;
-    
-}
+        alert("Preencha todos os campos!");
+        return;
+    }
 
-if (idade <= 0) {
-    alert("Idade inválida!");
-    return;
-}
-    let pessoa = {
-        nome: nome,
-        idade: idade
-    };
+    if (idade <= 0) {
+        alert("Idade inválida!");
+        return;
+    }
 
-    pessoas.push(pessoa);
+    if (editandoIndex !== null) {
+        pessoas[editandoIndex] = { nome, idade };
+        editandoIndex = null;
+    } else {
+        // NOVO CADASTRO
+        pessoas.push({ nome, idade });
+    }
 
     localStorage.setItem("pessoas", JSON.stringify(pessoas));
 
     atualizarLista();
+
     document.getElementById("nome").value = "";
     document.getElementById("idade").value = "";
 }
@@ -44,13 +57,13 @@ function atualizarLista() {
         let item = `
             <li>
                 ${pessoas[i].nome} - ${pessoas[i].idade} anos
+                <button onclick="editar(${i})">Editar</button>
                 <button onclick="remover(${i})">Remover</button>
             </li>
         `;
         lista.innerHTML += item;
     }
 }
-
 function remover(index) {
     pessoas.splice(index, 1);
     localStorage.setItem("pessoas", JSON.stringify(pessoas));
